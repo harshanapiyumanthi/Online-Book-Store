b3114861ece7c7e6a1f2f7daf69c0980f0d1393f
harshanapiyumanthi <harshanapiyumanthi9@gmail.com>
2026-05-12 18:27:29 +0530

    Update Book class

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,4 +1,18 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private String description;
+    private double price;
+    private String category;
+    private String bookType; // EBOOK, PRINTED
 }
