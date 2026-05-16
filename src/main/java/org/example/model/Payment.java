3af96510c620041b81d5b480af1b4d891492b8e8
IT25100011 <it25100011@my.sliit.lk>
2026-05-16 14:16:17 +0530

    Implement Payment model and repository

---
+++ src/main/java/org/example/model/Payment.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    private String id;
+    private String orderId;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    private String paymentDate;
+}
