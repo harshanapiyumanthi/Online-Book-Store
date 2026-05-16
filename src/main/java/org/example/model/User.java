7047522ff544e4ad437e5244e459e41d1d7cea9e
sabith536 <IT25100533@my.sliit.lk>
2026-05-16 23:45:17 +0530

    Add files via upload

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,18 @@
+package com.bookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String role; // CUSTOMER, ADMIN
+}
